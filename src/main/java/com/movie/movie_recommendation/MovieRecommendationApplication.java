e20bcb4ad553932c1da72a9328a79ca3416a3ed9
jaray22 <jaray22@ncsu.edu>
2024-06-20 22:07:52 -0400

    PUSH

--- src/main/java/com/movie/movie_recommendation/MovieRecommendationApplication.java
+++ src/main/java/com/movie/movie_recommendation/MovieRecommendationApplication.java
@@ -3,6 +3,9 @@ package com.movie.movie_recommendation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * HI 
+ */
 @SpringBootApplication
 public class MovieRecommendationApplication {
 
